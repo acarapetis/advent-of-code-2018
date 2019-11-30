2135b9a5b375708825f4cdfb8c32aa67dd9e7e93
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-11-30 13:12:20 +1100

    Rewrite problem 11 in numpy

--- problem11.py
+++ problem11.py
@@ -1,53 +1,26 @@
 #!/usr/bin/python3
 import sys
 import numpy as np
+from scipy.spatial.distance import cdist
 
-# This is super slow - for a "good" solution, either reimplement this approach
-# in a compiled language, or redesign it to leverage numpy/scipy rather than
-# loops.
-
-coords = [tuple(int(s.strip()) for s in l.strip().split(','))
+sources = [tuple(int(s.strip()) for s in l.strip().split(','))
           for l in sys.stdin.readlines()]
-xmax = max(x for x, y in coords)
-ymax = max(y for x, y in coords)
+xmax = max(x for x, y in sources)
+ymax = max(y for x, y in sources)
 shape = (xmax + 1, ymax + 1)
-R = max(*shape)
 
-grid = np.zeros(shape, dtype=int)
+# list of coordinates of grid tiles
+indices = np.asarray(list(np.ndindex(shape)))
+# dist[i,j] is distance from source i to tile j:
+dist = cdist(sources, indices, metric='cityblock')
+# mindist[j] is distance of closest source to tile j:
+mindist = np.min(dist, axis=0)
+# unique_minimizers[j] is number of sources achieving mindist to tile j:
+unique_minimizers = np.count_nonzero(dist == mindist, axis=0) == 1
 
-# Seed grid with initial points
-for i, c in enumerate(coords):
-    grid[c] = i + 1
-
-# Manhattan 1-neighbourhood
-def neighbourhood(x, y):
-    l = []
-    for a, b in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-        x1, y1  = x+a, y+b
-        if x1 < 0 or x1 > xmax or y1 < 0 or y1 > ymax:
-            continue
-        l.append(grid[x+a, y+b])
-    return l
-
-# Grow seed points gradually
-for r in range(R):
-    if np.all(grid != 0):
-        # we've determined the state of every tile; so we're done
-        break
-    new = grid.copy()
-    for x, y in np.ndindex(grid.shape):
-        if new[x, y] != 0:
-            # tile has already been claimed
-            continue
-        surrounds = {v for v in neighbourhood(x, y)
-                     if v != 0}
-        # If there's a unique closest point, it claims this tile
-        if len(surrounds) == 1:
-            new[x, y] = surrounds.pop()
-        # If there's more than one closest point, this tile becomes no-man's land
-        elif len(surrounds) > 1:
-            new[x, y] = -1
-    grid = new
+outvals = ((unique_minimizers-1) # -1 where minimizer is not unique
+           + unique_minimizers*(np.argmin(dist, axis=0)+1)) # index of minimizing source
+grid = np.reshape(outvals, shape)
 
 # If you spend a while doing some taxicab geometry, you can prove that the
 # infinite regions are exactly those that touch the boundary of the bounding
